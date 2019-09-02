edb1b01a86f4884ee1f642c5d7f81721b7314015
badubizzle <badu.boahen@gmail.com>
2019-09-02 23:06:03 +0000

    Integrate hypothesis into pytest

--- flaskgql/test/accounts_test.py
+++ flaskgql/test/accounts_test.py
@@ -1,83 +1,119 @@
 CREATE_ACCOUNT_MUTATION = """
-          mutation{{
-           createAccount(username: "{0}", token:"{1}"){{
-              account{{
-              ... on AccountObject{{
+          mutation createAccount($username: String!, $token: String!){
+           createAccount(username: $username, token: $token){
+              account{
+              ... on AccountObject{
                 uuid
                 balance
-              }}
-              ... on AuthInfoField{{
+              }
+              ... on AuthInfoField{
                     message
-              }}
-            }}
-          }}
-          }}
+              }
+            }
+          }
+          }
      """
 DEPOSIT_MUTATION = """
-     mutation{{
-       depositMoney(accountUuid:"{0}", amount: {2}, token:"{1}"){{
-         account{{
-           ... on AccountObject{{
+     mutation depositMoney($accountUuid: String!, $amount: Int!, $token: String!){
+       depositMoney(accountUuid: $accountUuid, amount: $amount, token: $token){
+         account{
+           ... on AccountObject{
              balance
              uuid
-           }}
-           ... on AuthInfoField{{
+           }
+           ... on AuthInfoField{
              message
-           }}
-         }}
-       }}
-     }}
+           }
+         }
+       }
+     }
      """
 
 WITHDRAW_MUTATION = """
-    mutation{{
-      withdrawMoney(accountUuid:"{0}", amount: {2}, token:"{1}"){{
-        account{{
-          ... on AccountObject{{
+    mutation withdrawMoney($accountUuid: String!, $amount: Int!, $token: String!){
+      withdrawMoney(accountUuid: $accountUuid, amount: $amount, token: $token){
+        account{
+          ... on AccountObject{
             balance
             uuid
-          }}
-          ... on AuthInfoField{{
+          }
+          ... on AuthInfoField{
             message
-          }}
-        }}
-      }}
-    }}
+          }
+        }
+      }
+    }
     """
 
 CREATE_USER_MUTATION = """
-     mutation{{
-          createUser(username: "{0}"){{
+     mutation createUser($username: String!) {
+          createUser(username: $username){
           accessToken
           refreshToken
-          user{{
+          user{
                username
-          }}
-          }}
-     }}
+          }
+          }
+     }
      """
-def test_create_accounts(client, gql, snapshot):
+from hypothesis import given
+import hypothesis.strategies as st
+from urllib.parse import quote
 
-    response = gql.mutate(CREATE_USER_MUTATION.format("Kwaku"))
+
+seen = set()
+@st.composite
+def list_unique_string(draw):    
+    data = st.text(min_size=5).filter()
+    return data
+
+
+@given(username=st.text(min_size=3))
+def test_create_accounts(username, client, gql, snapshot):
+
+    global seen
+
+    if username in seen:
+      return
+    seen.add(username)
+
+    response = gql.mutate(CREATE_USER_MUTATION, {'username': username})
     assert response.status_code == 200
+    assert 'data' in response.json 
+
+    print(response.json)
+    assert 'createUser' in response.json['data']  
     user = response.json['data']['createUser']
+
+    assert 'accessToken' in user
     token = user['accessToken']
+
+    assert 'user' in user
+    assert 'username' in user['user']
+
     username = user['user']['username']
 
-    response = gql.mutate(CREATE_ACCOUNT_MUTATION.format(username, token))
+    response = gql.mutate(CREATE_ACCOUNT_MUTATION, {'username': username,'token': token})
     assert response.status_code == 200
 
     # snapshot.assert_match(response.json)
 
+@given(username=st.text(min_size=5).map(lambda s: s.strip()),
+amount=st.integers(min_value=1, max_value=999999))
+def test_deposit_money(username, amount, client, gql, snapshot):
+    global seen
 
-def test_deposit_money(client, gql, snapshot):
-    response = gql.mutate(CREATE_USER_MUTATION.format("Kwame"))
+    if username in seen:
+      return
+    seen.add(username)
+    response = gql.mutate(CREATE_USER_MUTATION, {'username': username})
     assert response.status_code == 200
     user = response.json['data']['createUser']
     token = user['accessToken']
-    username = user['user']['username']
+    # username = user['user']['username']
+    assert username == user['user']['username']
 
-    response = gql.mutate(CREATE_ACCOUNT_MUTATION.format(username, token))
+    response = gql.mutate(CREATE_ACCOUNT_MUTATION, {'username': username, 'token': token})
     assert response.status_code == 200
     account = response.json['data']['createAccount']['account']
     print(account)
@@ -88,31 +124,51 @@ def test_deposit_money(client, gql, snapshot):
 
 
     account_id = account['uuid']
-    print("DEPOSIT===>", DEPOSIT_MUTATION.format(account_id, token, 100))
+    data = {'accountUuid': account_id, 'token': token,'amount': amount}
+    print("DEPOSIT===>", DEPOSIT_MUTATION, data)
     print(account_id)
-    response = gql.mutate(DEPOSIT_MUTATION.format(account_id, token, 100))
+    response = gql.mutate(DEPOSIT_MUTATION, data)
 
     assert response.status_code == 200
-    print(response.json)
+    print("DEPOSIT RESPONSE===>>>", response.json)
+
     updated_account = response.json['data']['depositMoney']['account']
     assert 'uuid' in updated_account
     assert updated_account['uuid'] == account['uuid']
     assert 'balance' in updated_account
-    assert updated_account['balance'] == 100
+    assert updated_account['balance'] == amount
 
     # snapshot.assert_match(response.json)
 
-def test_withdraw_money(client, gql, snapshot):
+@st.composite
+def deposit_withdrawal_pair(draw):
+  deposit_amount=draw(st.integers(min_value=1, max_value=999999))
+  withdrawal_amount=draw(st.integers(min_value=1, max_value = deposit_amount))
+  
 
-    response = gql.mutate(CREATE_USER_MUTATION.format("Kojo"))
+  return deposit_amount, withdrawal_amount
+
+@given(username=st.text(min_size=5).map(lambda s: s.strip()), amounts = deposit_withdrawal_pair())
+def test_withdraw_money(username, amounts, client, gql, snapshot):
+
+    global seen
+
+    if username in seen:
+      return
+    seen.add(username)
+
+    deposit_amount, withdrawal_amount = amounts
+    response = gql.mutate(CREATE_USER_MUTATION, {'username': username})
     assert response.status_code == 200
     user = response.json['data']['createUser']
     token = user['accessToken']
-    username = user['user']['username']
+    # username = user['user']['username']
+    assert username == user['user']['username']
 
-    response = gql.mutate(CREATE_ACCOUNT_MUTATION.format(username, token))
+    response = gql.mutate(CREATE_ACCOUNT_MUTATION, {'username': username, 'token': token})
     assert response.status_code == 200
     account = response.json['data']['createAccount']['account']
+    print(account)
     assert 'uuid' in account
     assert 'balance' in account
     assert account['balance'] == 0
@@ -120,22 +176,25 @@ def test_withdraw_money(client, gql, snapshot):
 
 
     account_id = account['uuid']
-    response = gql.mutate(DEPOSIT_MUTATION.format(account_id, token, 100))
+    print("DEPOSIT===>", DEPOSIT_MUTATION)
+    print(account_id)
+    response = gql.mutate(DEPOSIT_MUTATION, {'accountUuid': account_id, 'token': token,'amount': deposit_amount})
 
     assert response.status_code == 200
+    print(response.json)
     updated_account = response.json['data']['depositMoney']['account']
     assert 'uuid' in updated_account
     assert updated_account['uuid'] == account['uuid']
     assert 'balance' in updated_account
-    assert updated_account['balance'] == 100
+    assert updated_account['balance'] == deposit_amount
 
 
-    response = gql.mutate(WITHDRAW_MUTATION.format(account_id, token, 20))
+    response = gql.mutate(WITHDRAW_MUTATION, {'accountUuid': account_id, 'token': token,'amount': withdrawal_amount})
 
     assert response.status_code == 200
     updated_account = response.json['data']['withdrawMoney']['account']
     assert 'uuid' in updated_account
     assert updated_account['uuid'] == account['uuid']
     assert 'balance' in updated_account
-    assert updated_account['balance'] == 80
+    assert updated_account['balance'] == deposit_amount - withdrawal_amount
 
--- flaskgql/test/user_test.py
+++ flaskgql/test/user_test.py
@@ -1,17 +1,27 @@
-def test_create_user_with_user_name(client,gql, snapshot):
 
-     mutation = """
-     mutation{
-  createUser(username: "Kofi"){
-    accessToken
-    refreshToken
-    user{
-      username
-    }
-  }
-}
-     """ 
-     response = gql.mutate(mutation)     
-     # snapshot.assert_match(response.data)     
-     assert response.status_code == 200
+from hypothesis import given
+import hypothesis.strategies as st
+from urllib.parse import quote
+
+CREATE_USER_MUTATION = """
+     mutation createUser($username: String!) {
+          createUser(username: $username){
+          accessToken
+          refreshToken
+          user{
+               username
+          }
+          }
+     }
+     """
+
+@given(username=st.text(min_size=3)
+#.filter(lambda s: len([c for c in s if not c.isalnum()]) == 0) 
+.map(lambda s: s.strip()))
+def test_create_user_with_user_name(username, client, gql, snapshot):        
+    mutation = CREATE_USER_MUTATION
+    variables = {'username': username}
+    response = gql.mutate(mutation, variables)     
+    print("USER===>>", response.json)
+    assert response.status_code == 200, (mutation, response.data)
      
